8efc8578264d90e6dd91b8b22a056802a628efff
stevmoli <stevmoli805@gmail.com>
2015-10-05 21:30:40 -0400

    Completing initial heuristic calculation

--- search/SearchBase.java
+++ search/SearchBase.java
@@ -56,8 +56,12 @@ public class SearchBase {
 		public char[][] manhattanFinderStart = new char[slashes+1][slashes+1];
 		public char[][] manhattanFinderGoal = new char[slashes+1][slashes+1];
 
+		// The following two lines take the start and goal states and put them into the 2D arrays created above
 		manhattanFinderStart = cleanArray(start, manhattanFinderStart);
 		manhattanFinderGoal = cleanArray(goal, manhattanFinderGoal);
+
+		// The heuristic will be the sum of the manhattan distances of each number in the puzzle from its goal state
+		heuristic = calcHeuristic(manhattanFinderStart, manhattanFinderGoal); // calculates the initial heuristic
 
 		open.add(new State(ssp.getStart(), heuristic));
 
@@ -108,7 +112,7 @@ public class SearchBase {
 		return search(done,limit,ssp,new PQasList());
 	}
 
-	public char[][] cleanArray(String status, char[][] target) {
+	public char[][] cleanArray(String status, char[][] target) { // converts start and goal state strings into 2D arrays that will be used to calculate the heuristic
 
 		int j = 0; // first index of 2D array
 		int k = 0; // second index of 2D array
@@ -128,6 +132,38 @@ public class SearchBase {
 		}
 
 		return target;
+	}
+
+	public int calcHeuristic(char[][] start, char[][] goal) {
+		int heuristic = 0;
+		// First we see how many of the outer level arrays we must traverse to get a number from it's start state to goal state.
+		// For every one of these outer array traversals (equivalent to a vertical move in the puzzle), we add 1 to the heuristic.
+		// Then we also check the index of a number in the inner array in each state (equivalent to a horizontal move in the puzzle).
+		// The value of this difference will also be added to the heuristic.
+		//The sum of these two numbers gives us the manhattan distance of a particular number in the puzzle.
+		for (int i = 0; i < start.length-1; i++) { // Iterating through outer arrays of start state
+			for (int j = 0; j < start.length-1; j++) { // Iterating through indicies of each outer array of start state
+				boolean found = false;  // will break us out of while loop once we have calculated the heuristic of a number in the puzzle
+				//int nextArr = 0; // will allow us to iterate through the 2D goal array without using another set of nested for loops
+				int k = 0; // first index of goal 2D array
+				int l = 0; // second index of goal 2D array
+				while (found == false) {
+					if (start[i][j] == goal[k][l]) {  // True if we have found a number in both the start and goal states
+						heuristic += Math.abs(k-i)+Math.abs(l-j); // adding the manhattan dist. of this number to the heuristic value
+						found = true; // break out of the while loop, then we move on to the next number in the start array
+					}
+					l++;  // increments index of inner goal array
+					if (l >= 3) { // iterating through goal array without nested for loops
+						l = 0;
+						k++; // increments index of outer goal array (upon reaching the end of the current inner goal array)
+					}
+					if (k >= 3) {
+						throw new Exception("Error determining heuristic: reached end of goal state without finding item from start state");
+					}
+				}
+			}
+		}
+		return heuristic;
 	}
 
 
